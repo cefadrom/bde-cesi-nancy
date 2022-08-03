b575ec38b91cd32a4edfe91246f091f26bcb81c5
Julien <cefadrom1@gmail.com>
2022-08-03 22:39:57 +0200

    feat: styles to stories and new backgrounds

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,22 +1,26 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  webpackFinal: async (config) => {
-    const svelteLoader = config.module.rules.find((r) => r.loader && r.loader.includes('svelte-loader'))
-    svelteLoader.options.preprocess = require('svelte-preprocess')()
-    return config
-  },
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-    "@hover/storybook-addon-pseudo-states"
-  ],
-  framework: "@storybook/svelte",
-  features: {
-    interactionsDebugger: true,
-  },
-}
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
+    ],
+    webpackFinal: async (config) => {
+        const svelteLoader = config.module.rules.find((r) => r.loader && r.loader.includes('svelte-loader'));
+        svelteLoader.options.preprocess = require('svelte-preprocess')();
+        return config;
+    },
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-a11y',
+        '@hover/storybook-addon-pseudo-states'
+    ],
+    framework: '@storybook/svelte',
+    features: {
+        interactionsDebugger: true,
+    },
+    staticDirs: [
+        '../../../web/static',
+        '../../../web/src/lib',
+    ]
+};
--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,9 +1,17 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on:[a-z]" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on:[a-z]' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+    backgrounds: {
+        default: 'Light',
+        values: [
+            { name: 'Light', value: 'var(--white)' },
+            { name: 'Dark', value: 'var(--black)' },
+            { name: 'Image', value: 'url(students/students-1440.jpg)' },
+        ],
+    }
+};
