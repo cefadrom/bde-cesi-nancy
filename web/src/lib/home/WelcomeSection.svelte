4ff123c5bc8ad7931c6e6931e3e9415396658554
Julien <cefadrom1@gmail.com>
2022-08-04 22:58:54 +0200

    feat: separate page into components

---
+++ web/src/lib/home/WelcomeSection.svelte
@@ -0,0 +1,29 @@
+<style>
+    img {
+        height: 100vh;
+        width: 100%;
+        object-fit: cover;
+        object-position: 15% 90%;
+    }
+
+    h1 {
+        position: absolute;
+        left: 2rem;
+        bottom: 2rem;
+        color: var(--white);
+        max-width: 11em;
+        margin-right: 2em;
+    }
+</style>
+
+
+<section>
+    <picture>
+        <source media="(min-width: 1920px) or (min-height: 1080px)" srcset="students/students-1440.jpg">
+        <source media="(min-width: 1080px) or (min-height: 720px)" srcset="students/students-1920.jpg">
+        <source media="(max-width: 1080px) and (max-height: 720px)" srcset="students/students-720.jpg">
+        <img alt="Étudiants" src="students/students-1920.jpg">
+    </picture>
+
+    <h1 class="header-1">Bienvenue sur le site du BDE du CESI Nancy</h1>
+</section>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,34 +1,12 @@
 <script>
     import Meta from '$lib/Meta.svelte';
+    import WelcomeSection from '$lib/home/WelcomeSection.svelte';
 </script>
-
-<style>
-    img {
-        height: 100vh;
-        width: 100%;
-        object-fit: cover;
-        object-position: 15% 90%;
-    }
-
-    h1 {
-        position: absolute;
-        left: 2rem;
-        bottom: 2rem;
-        color: var(--white);
-        max-width: 11em;
-        margin-right: 2em;
-    }
-</style>
 
 
 <Meta/>
 
 
-<picture>
-    <source srcset="students/students-1440.jpg" media="(min-width: 1920px) or (min-height: 1080px)">
-    <source srcset="students/students-1920.jpg" media="(min-width: 1080px) or (min-height: 720px)">
-    <source srcset="students/students-720.jpg" media="(max-width: 1080px) and (max-height: 720px)">
-    <img src="students/students-1920.jpg" alt="Étudiants">
-</picture>
-
-<h1 class="header-1">Bienvenue sur le site du BDE du CESI Nancy</h1>
+<main>
+    <WelcomeSection/>
+</main>
