413ca21000da8235ad55aa4c2d59dea29c8f11db
Julien <cefadrom1@gmail.com>
2022-08-05 14:50:18 +0200

    feat: move the main to the layout

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -3,5 +3,15 @@
     import Header from '@bde-cesi-nancy/components/src/Header/Header.svelte';
 </script>
 
+
+<style>
+    main {
+        min-height: 100vh;
+    }
+</style>
+
+
 <Header/>
-<slot/>
+<main>
+    <slot/>
+</main>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -8,7 +8,5 @@
 <Meta/>
 
 
-<main>
-    <WelcomeSection/>
-    <AboutSection/>
-</main>
+<WelcomeSection/>
+<AboutSection/>
