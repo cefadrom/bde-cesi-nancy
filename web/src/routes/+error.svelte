9902d6c4918a0d19a6145850d27a5e3f5dd0914a
Julien W <cefadrom1@gmail.com>
2022-08-17 18:43:20 +0200

    chores: migrate sveltekit routing

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -1,28 +1,14 @@
-<script context="module" lang="ts">
-    import type { Load } from '@sveltejs/kit';
-
-    export const load: Load = ({ status, error }) => {
-        return {
-            props: {
-                status,
-                error,
-            },
-        };
-    };
-</script>
-
-
 <script lang="ts">
     import Button from '@bde-cesi-nancy/components/src/Button/Button.svelte';
     import { onMount } from 'svelte';
     import { getErrorMessage } from '$lib/errorMessage.js';
     import Meta from '$lib/Meta.svelte';
-
-    export let status: number;
-    export let error: Error = new Error('Unknown error');
+    import { page } from '$app/stores';
 
     onMount(() => {
-        console.error(error instanceof Error || (error as Error).stack ? error.stack : error);
+        console.error($page.error instanceof Error || ($page.error as Error).stack
+            ? ($page.error as Error).stack
+            : $page.error);
     });
 
     // Stack trace display - click 5 times on the status code in 4 seconds
@@ -35,7 +21,7 @@
                 statusCodeClicks = 0;
             }, 4000);
         if (statusCodeClicks === 5)
-            alert(JSON.stringify(error, null, 2).replace(/\\n/g, '\n'));
+            alert(JSON.stringify($page.error, null, 2).replace(/\\n/g, '\n'));
     }
 </script>
 
@@ -70,12 +56,12 @@
 </style>
 
 
-<Meta noindex title="{error.name} {status}"/>
+<Meta noindex title="{$page.error.name} {$page.status}"/>
 
 
 <div class="error-container">
-    <h2 class="header-2" on:click={handleStatusCodeClick}>{error.name} {status}</h2>
-    <h3 class="h3">{getErrorMessage(status, error)}</h3>
+    <h2 class="header-2" on:click={handleStatusCodeClick}>{$page.error.name} {$page.status}</h2>
+    <h3 class="h3">{getErrorMessage($page.status, $page.error)}</h3>
     <p class="body">Deux options s'offrent à vous. Faites le bon choix.</p>
     <a href="/">
         <Button icon="home-filled-white">Retourner à l'accueil</Button>
