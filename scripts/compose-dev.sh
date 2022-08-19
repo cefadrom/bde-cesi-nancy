929495cbc06ede23c1c9e2969fe449a15cacb117
Julien W <cefadrom1@gmail.com>
2022-08-19 14:35:43 +0200

    feat: default services if not specified

--- scripts/compose-dev.sh
+++ scripts/compose-dev.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+# shellcheck disable=SC2039,SC2128
 
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d "$@"
+services=("$@")
+
+if [ -z "$services" ]; then
+  services=("directus" "mysql" "redis")
+fi
+
+echo "Starting services:" "${services[@]}"
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d "${services[@]}"
