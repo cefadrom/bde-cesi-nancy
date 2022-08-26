cd665b7566ffee6181728c7db84c20f87c900a4e
Julien <cefadrom1@gmail.com>
2022-08-26 19:01:57 +0200

    feat: consistent formatting of select

--- packages/components/src/Select/Select.svelte
+++ packages/components/src/Select/Select.svelte
@@ -10,13 +10,38 @@
 </script>
 
 
+<style>
+    select {
+        appearance: initial;
+        color: inherit;
+    }
+
+    .select-wrapper {
+        position: relative;
+    }
+
+    .select-wrapper::after {
+        content: "";
+        background-image: url('/icons/caret-down-filled-black.svg');
+        height: 1rem;
+        width: 1rem;
+        top: 50%;
+        right: .9rem;
+        position: absolute;
+        transform: translateY(-50%);
+    }
+</style>
+
+
 <InputContainer {...$$props} let:handleBlur let:props>
-    <select {...props} bind:value on:blur={handleBlur} on:change>
-        {#if defaultChoice}
-            <option disabled selected value={null}>{defaultChoice}</option>
-        {/if}
-        {#each Object.entries(options) as [key, value] (key)}
-            <option value={key}>{value}</option>
-        {/each}
-    </select>
+    <div class="select-wrapper">
+        <select {...props} bind:value on:blur={handleBlur} on:change>
+            {#if defaultChoice}
+                <option disabled selected value={null}>{defaultChoice}</option>
+            {/if}
+            {#each Object.entries(options) as [key, value] (key)}
+                <option value={key}>{value}</option>
+            {/each}
+        </select>
+    </div>
 </InputContainer>
