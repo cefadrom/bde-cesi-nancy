703107ad6a8ab97c130cd11eb8deddf8d8bb9009
Julien <cefadrom1@gmail.com>
2022-09-10 17:27:59 +0200

    feat: temporary events page

---
+++ web/src/routes/events/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import SectionContainer from '$lib/layout/SectionContainer.svelte';
+    import Meta from '$lib/Meta.svelte';
+</script>
+
+
+<Meta title="Évènements"/>
+
+
+<SectionContainer header>
+    <h1 class="header-1">Évènements 🎉</h1>
+    <p class="body">C'est sur cette page que vous verrez les évènements passés et à venir 👀</p>
+</SectionContainer>
