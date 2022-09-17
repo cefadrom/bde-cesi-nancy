047091f255d5e3193ffb4e2bc54ff26bf63e2cc5
Julien <cefadrom1@gmail.com>
2022-09-17 14:02:19 +0200

    feat: hide module for non-admins

--- directus/src/modules/cesi-accounts/index.ts
+++ directus/src/modules/cesi-accounts/index.ts
@@ -6,6 +6,7 @@ export default {
     id: 'cesi-accounts',
     name: 'Comptes CESI',
     icon: 'admin_panel_settings',
+    preRegisterCheck: user => user.role.admin_access,
     routes: [
         {
             path: '',
