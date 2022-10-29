717c058b3642da4fbc7da7ace9144df3ccec52ae
Julien <cefadrom1@gmail.com>
2022-10-29 14:16:31 +0200

    feat: page meta

--- web/src/routes/member-dashboard/settings/+page.svelte
+++ web/src/routes/member-dashboard/settings/+page.svelte
@@ -1,7 +1,12 @@
 <script>
     import SectionContainer from '$lib/layout/SectionContainer.svelte';
     import NotificationSettings from '$lib/member-dashboard/settings/NotificationSettings.svelte';
+    import Meta from '$lib/Meta.svelte';
 </script>
+
+
+<Meta title="Paramètres utilisateur"/>
+
 
 <SectionContainer header>
     <h2 class="header-2">Paramètres utilisateur ⚙️</h2>
