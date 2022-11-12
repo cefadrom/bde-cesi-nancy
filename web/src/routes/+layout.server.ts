1a09e343e058d42406c4e0a5eb5ac5c4585beeda
Julien <cefadrom1@gmail.com>
2022-11-12 23:15:16 +0100

    feat: user profile SSR

--- web/src/routes/+layout.server.ts
+++ web/src/routes/+layout.server.ts
@@ -1,7 +1,53 @@
+import { dev } from '$app/environment';
+import { env as _env } from '$env/dynamic/private';
+import { env } from '$env/dynamic/public';
+import { getUserProfile } from '$lib/api/getUserProfile';
+import type { UserProfile } from '$lib/types';
+import { Directus, MemoryStorage } from '@directus/sdk';
 import type { ServerLoad } from '@sveltejs/kit';
 
-export const load: ServerLoad = ({ cookies }) => {
+const { PUBLIC_DIRECTUS_URL } = env;
+const { REFRESH_TOKEN_COOKIE_DOMAIN } = _env;
+
+export interface RootLayoutLoad {
+    me: UserProfile | null;
+}
+
+export const load: ServerLoad = async ({ cookies, request }) => {
     const refreshToken = cookies.get('directus_refresh_token');
 
-    return { hasRefreshToken: !!refreshToken };
+    let me: null | UserProfile = null;
+
+    // Cannot use the `url` from sveltekit, or it will trigger a call to this load function
+    // when navigating inside the app
+    if (refreshToken && new URL(request.url).pathname !== '/login') {
+        try {
+            const directus = new Directus(PUBLIC_DIRECTUS_URL!, {
+                storage: new MemoryStorage(),
+            });
+            directus.storage.set('auth_refresh_token', refreshToken);
+            const res = await directus.auth.refresh();
+
+            if (!res) {
+                throw new Error('Cannot refresh token');
+            }
+
+            me = await getUserProfile(directus);
+
+            cookies.set(
+                'directus_refresh_token',
+                res.refresh_token!,
+                {
+                    domain: REFRESH_TOKEN_COOKIE_DOMAIN,
+                    path: '/',
+                    httpOnly: true,
+                    secure: !dev,
+                    sameSite: dev ? 'lax' : 'none',
+                },
+            );
+        } catch {
+        }
+    }
+
+    return { me } as RootLayoutLoad;
 };
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -8,33 +8,30 @@
     import { Directus } from '@directus/sdk';
     import { onMount, setContext } from 'svelte';
     import { writable } from 'svelte/store';
-    import { getUserProfile } from '$lib/api/getUserProfile';
+    import type { RootLayoutLoad } from './+layout.server';
 
-    export let data: { hasRefreshToken: boolean };
+    export let data: RootLayoutLoad = { me: null };
 
     const directus = new Directus(env.PUBLIC_DIRECTUS_URL);
-    const me = writable<UserProfile | null>(null);
-    const loginStatus = writable<LoginStatus>('LOGGING_IN');
+    const me = writable<UserProfile | null>(data.me);
+    const loginStatus = writable<LoginStatus>(data.me ? 'LOGGED_IN' : 'LOGGED_OUT');
 
     setContext('directus', directus);
     setContext('me', me);
     setContext('loginStatus', loginStatus);
 
-    // Tries to connect if a valid token is found
+    // Refresh the token for the logged-in user, so directus can access the API
     onMount(async () => {
+        // Don't try to refresh the token if we're not logged in
+        if ($loginStatus === 'LOGGED_OUT')
+            return;
+
         // Don't refresh if we're on the login page, let it handle it
         if ($page.url.pathname === '/login')
             return;
 
-        // Don't even try to refresh if there is no refresh cookie
-        if (!data.hasRefreshToken) {
-            loginStatus.set('LOGGED_OUT');
-            return;
-        }
-
         try {
             await directus.auth.refresh();
-            $me = await getUserProfile(directus);
             $loginStatus = 'LOGGED_IN';
         } catch {
             $loginStatus = 'LOGGED_OUT';
