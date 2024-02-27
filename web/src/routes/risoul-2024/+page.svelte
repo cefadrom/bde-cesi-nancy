1895e906c069b498c0a5642ea48e5f78b51df01e
Julien W <50249422+julien-wff@users.noreply.github.com>
2024-02-27 22:34:37 +0100

    feat: register button and hook section

--- web/src/routes/risoul-2024/+page.svelte
+++ web/src/routes/risoul-2024/+page.svelte
@@ -1,9 +1,13 @@
 <script>
     import Meta from "$lib/Meta.svelte";
     import IntroSection from "$lib/risoul-2024/IntroSection.svelte";
+    import RegisterButton from "$lib/risoul-2024/RegisterButton.svelte";
+    import HookSection from "$lib/risoul-2024/HookSection.svelte";
 </script>
 
 
 <Meta title="Week-end au ski" description="En mars, venez passer un week-end inoubliable dans les Alpes avec le BDE !"/>
 
 <IntroSection/>
+<RegisterButton/>
+<HookSection/>
