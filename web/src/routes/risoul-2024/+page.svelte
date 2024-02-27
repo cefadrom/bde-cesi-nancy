70102bf81c8d4d326b44d460e80d98acc84ae99e
Julien W <50249422+julien-wff@users.noreply.github.com>
2024-02-27 19:27:00 +0100

    feat: page and intro picture

---
+++ web/src/routes/risoul-2024/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Meta from "$lib/Meta.svelte";
+    import IntroSection from "$lib/risoul-2024/IntroSection.svelte";
+</script>
+
+
+<Meta title="Week-end au ski" description="En mars, venez passer un week-end inoubliable dans les Alpes avec le BDE !"/>
+
+<IntroSection/>
